229d369d090503845716ab0fcc5fc0244212a4d2
hbtao <hbtao18062547611@foxmail.com>
2020-07-15 19:40:43 +0800

    项目逆向完成

---
+++ gmall-api/src/main/java/com/atguigu/gmall/ums/service/IntegrationConsumeSettingService.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.ums.service;
+
+import com.atguigu.gmall.ums.entity.IntegrationConsumeSetting;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 积分消费设置 服务类
+ * </p>
+ *
+ * @author hbtao
+ * @since 2020-07-12
+ */
+public interface IntegrationConsumeSettingService extends IService<IntegrationConsumeSetting> {
+
+}
---
+++ gmall-mbg/src/main/java/com/atguigu/gmall/pms/service/ProductCategoryService.java
@@ -0,0 +1,16 @@
+package com.atguigu.gmall.pms.service;
+
+import com.atguigu.gmall.pms.entity.ProductCategory;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 产品分类 服务类
+ * </p>
+ *
+ * @author hbtao
+ * @since 2020-07-12
+ */
+public interface ProductCategoryService extends IService<ProductCategory> {
+
+}
